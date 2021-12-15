77d25e27a8758c6731ae575d19ab3ccbd72db0b1
uelissonpda <uelissonpda@hotmail.com>
2021-12-15 14:11:33 -0300

    Add files via upload

--- src/co/fcode/sjtged/sistema/Database.java
+++ src/co/fcode/sjtged/sistema/Database.java
@@ -14,12 +14,15 @@ public class Database implements Runnable{
 	
 	/* Conexão com SQL */
 	// -- Acesso ao Banco da Alterdata (Produção)
-	public static String urlAlterdata = "jdbc:sqlserver://192.168.100.200:54657;databaseName=ALTERDATA"; // @production
+	public static String urlAlterdata = "jdbc:sqlserver://192.168.100.220:64363;databaseName=ALTERDATA_PACK"; // @production
+	//public static String urlAlterdata = "jdbc:sybase:Tds:192.168.100.216:2638?ServiceName=contabil"; // @production
 	//public static String urlAlterdata = "jdbc:sqlserver://177.207.207.148:49398;databaseName=ALTERDATA"; // @home
 	// SQL RESERVA
 	//public static String urlAlterdata = "jdbc:sqlserver://192.168.100.204:49996;databaseName=ALTERDATA";
 	public static String userDBAlterdata = "sa";
 	public static String passDBAlterdata = "#abc123#";
+	//public static String userDBAlterdata = "externo"; dominio
+	//public static String passDBAlterdata = "externo"; dominio
 
 	@Override
 	public void run() {}
